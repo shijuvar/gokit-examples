db93626467ba239a435c022328a20ead8a3b55ce
mario <ghyang08@163.com>
2022-09-27 17:50:31 +0800

    Update main.go
    fix:os.Signal chan

--- services/order/cmd/main.go
+++ services/order/cmd/main.go
@@ -94,7 +94,7 @@ func main() {
 
 	errs := make(chan error)
 	go func() {
-		c := make(chan os.Signal)
+		c := make(chan os.Signal, 1)
 		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 		errs <- fmt.Errorf("%s", <-c)
 	}()
